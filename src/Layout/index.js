f38ddf366bc7517abb5af424a71f4b75728908ae
jushih <jvshih@gmail.com>
2022-02-21 23:29:06 -0800

    Add study deck feature

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,19 +2,15 @@ import React from "react";
 import { Fragment } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
-import CreateDeckButton from "./CreateDeckButton"
-import CreateDeck from "./CreateDeck"
-import DeckList from "./DeckList"
-import StudyDeck from "./StudyDeck"
-import ViewDeck from "./ViewDeck"
-import AddCard from "./Card/AddCard"
-import EditCard from "./Card/EditCard"
-import EditDeck from "./EditDeck"
-import {
-  Route,
-  Switch
-} from "react-router-dom";
-
+import CreateDeckButton from "./CreateDeckButton";
+import CreateDeck from "./CreateDeck";
+import DeckList from "./DeckList";
+import StudyDeck from "./StudyDeck";
+import ViewDeck from "./ViewDeck";
+import AddCard from "./Card/AddCard";
+import EditCard from "./Card/EditCard";
+import EditDeck from "./EditDeck";
+import { Route, Switch } from "react-router-dom";
 
 function Layout() {
   return (
@@ -22,44 +18,41 @@ function Layout() {
       <Header />
       <div className="container">
         <Switch>
-          
-         <Route exact path={`/`}>
-           <CreateDeckButton />
-           <DeckList />
-         </Route>
+          <Route exact path={`/`}>
+            <CreateDeckButton />
+            <DeckList />
+          </Route>
 
-         <Route path="/decks/new">
-           <CreateDeck />
-         </Route>
+          <Route path="/decks/new">
+            <CreateDeck />
+          </Route>
 
-         <Route path="/decks/:deckId/study">
-           <StudyDeck />
-         </Route>
+          <Route path="/decks/:deckId/study">
+            <StudyDeck />
+          </Route>
 
-         <Route path="/decks/:deckId/edit">
-           <EditDeck />
-         </Route>
+          <Route path="/decks/:deckId/edit">
+            <EditDeck />
+          </Route>
 
-         <Route path="/decks/:deckId/cards/new">
-           <AddCard />
-         </Route>
+          <Route path="/decks/:deckId/cards/new">
+            <AddCard />
+          </Route>
 
-         <Route path="/decks/:deckId/cards/:cardId/edit">
-           <EditCard />
-         </Route>
+          <Route path="/decks/:deckId/cards/:cardId/edit">
+            <EditCard />
+          </Route>
 
-         <Route path="/decks/:deckId/">
-           <ViewDeck />
-         </Route>
+          <Route path="/decks/:deckId/">
+            <ViewDeck />
+          </Route>
 
           <Route>
-          <NotFound />
+            <NotFound />
           </Route>
-          
-       </Switch>
+        </Switch>
       </div>
     </Fragment>
-    
   );
 }
 
